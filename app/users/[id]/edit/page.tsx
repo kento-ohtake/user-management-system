51db6e0a47887b1ffb74486bbc241778b667c6a8
kento-ohtake <kento.ohtake@fox-hound.jp>
2025-04-09 12:50:49 +0900

    task2-3 完了

--- app/users/[id]/edit/page.tsx
+++ app/users/[id]/edit/page.tsx
@@ -4,17 +4,20 @@
 
 import EditUserForm from "@/components/EditUserForm";
 import { Box, Typography } from "@mui/material";
+import { useParams } from "next/navigation";
 import React from "react";
 
 // TODO: URLパラメータからユーザーIDを取得し、EditUserFormコンポーネントに渡す
 const EditUserPage: React.FC = () => {
+  const { id } = useParams();
+  
   return (
     <Box sx={{ mt: 4 }}>
       <Typography variant="h4" gutterBottom>
         ユーザー編集
       </Typography>
       <EditUserForm
-        userId={1} // TODO: URLパラメータから取得したユーザーIDを渡す
+        userId={Number(id)} // TODO: URLパラメータから取得したユーザーIDを渡す
         disabled={false}
       />
     </Box>
