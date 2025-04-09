dc2146b9562a5c51c969d0bd139b2fae360ecd5f
kento-ohtake <kento.ohtake@fox-hound.jp>
2025-04-09 10:55:18 +0900

    タスク1-3 新規登録ページの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Typography, Box } from '@mui/material';
-import RegisterForm from '@/components/RegisterForm';
+import React from "react";
+import { Typography, Box } from "@mui/material";
+import RegisterForm from "@/components/RegisterForm";
 
 const HomePage: React.FC = () => {
   return (
@@ -11,9 +11,8 @@ const HomePage: React.FC = () => {
       <Typography variant="h6" color="text.secondary">
         このシステムでは、ユーザーの一覧表示、編集、削除が可能です。
       </Typography>
-      <RegisterForm />
     </Box>
   );
-}
+};
 
 export default HomePage;
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,16 +1,31 @@
 // app/register/page.tsx
+"use client"; // クライアントコンポーネントとしてマーク
 
-'use client'; // クライアントコンポーネントとしてマーク
-
-import React from 'react';
-import RegisterForm from '../../components/RegisterForm';
-import { Typography, Box } from '@mui/material';
+import React from "react";
+import { useRouter } from "next/navigation";
+import RegisterForm from "../../components/RegisterForm";
+import { Box } from "@mui/material";
+import Navbar from "@/components/Navbar";
 
 // TODO: 新規登録ページを実装し、RegisterFormコンポーネントを使用する
 const RegisterPage: React.FC = () => {
+  const router = useRouter();
+
   return (
-  //  ここにユーザー一覧へのルーターを追加する
+    //  ここにユーザー一覧へのルーターを追加する
+    <>
+      <Box>
+        <RegisterForm
+          onSuccess={() => {
+            alert("登録が成功しました！");
+            router.push("/users");
+          }}
+          onError={() => alert("エラーが発生しました")}
+          disabled={false}
+        />
+      </Box>
+    </>
   );
-}
+};
 
 export default RegisterPage;
