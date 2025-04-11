ee10c21bcdb3df3c2cd2d873e99388ef9e5202e1
kento-ohtake <kento.ohtake@fox-hound.jp>
2025-04-11 12:15:08 +0900

    day3_task2-1完了

---
+++ components/parts/CustomCard.tsx
@@ -0,0 +1,32 @@
+// components/parts/CustomCard.tsx
+
+import React from "react";
+import { Card, CardContent, Typography, CardActions } from "@mui/material";
+
+// TODO: インターフェースを修正
+interface CustomCardProps {
+  title: string;
+  description: string;
+  actions?: React.ReactNode; //調べる　
+}
+
+const CustomCard: React.FC<CustomCardProps> = ({
+  title,
+  description,
+  actions,
+}) => {
+  return (
+    <Card sx={{ minWidth: 275, mb: 2 }}>
+      <CardContent>
+        {/* TODO: [titel]と[description]を表示 */}
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2">{description}</Typography>
+      </CardContent>
+      {actions && <CardActions>{actions}</CardActions>}
+    </Card>
+  );
+};
+
+export default CustomCard;
