0a1d025daa640e0d021c93bd6681c37f69cfb6ce
kento-ohtake <kento.ohtake@fox-hound.jp>
2025-04-11 10:44:07 +0900

    task1-3 ストーリーブックの作成完了

---
+++ components/parts/CustomButton.stories.tsx
@@ -0,0 +1,38 @@
+// components/parts/CustomButton.stories.tsx
+
+import type { Meta, StoryObj } from "@storybook/react";
+import CustomButton from "./CustomButton";
+
+const meta: Meta<typeof CustomButton> = {
+  title: "Components/Parts/CustomButton",
+  component: CustomButton,
+  tags: ["autodocs"], //まとめた一覧
+};
+
+// TODO: メタデータのエクスポート
+export default meta;
+
+// TODO: ストーリーの定義
+type Story = StoryObj<typeof CustomButton>;
+
+// TODO: 上記サンプルを参考に[Secondary][Danger]を設定する
+export const Primary: Story = {
+  args: {
+    variantType: "primary",
+    children: "Primary Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variantType: "secondary",
+    children: "Secondary Button",
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    variantType: "danger",
+    children: "Danger Button",
+  },
+};
